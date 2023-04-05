058be0449fb995d336cce0f8e11f9215616f807b
Sergey Shvets W <recycletechno@gmail.com>
2023-04-06 01:30:48 +0300

    Update tests

--- test_app.py
+++ test_app.py
@@ -1,10 +1,12 @@
 # tests/test_app.py
 import unittest
 from app import app
+from fastapi.testclient import TestClient
 
-class TestApp(unittest.TestCase):
-    def test_predict_sentiment(self):
-        with app.test_client() as client:
-            response = client.post('/predict', json={'text': 'This is a great movie!'})
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json, {'sentiment': 'positive'})
+client = TestClient(app)
+
+
+def get_test(get_client: TestClient):
+    response = get_client.post('/predict', json={'text': 'This is a great movie!'})
+    assert response.status_code == 200
+    assert response.json == {'sentiment': 'positive'}
