707cfc252d97014885c9ffce23f3aa042560a23d
Sergey Shvets W <recycletechno@gmail.com>
2023-04-06 01:15:41 +0300

    Update app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from get_sentiment import get_sentiment_fx
+# import uvicorn
 
 app = FastAPI()
 
@@ -15,8 +16,11 @@ def read_root():
 @app.post("/predict")
 async def predict_sentiment(input_text: InputText):
 
-    sentiment = get_sentiment(input_text.text)
+    sentiment = get_sentiment_fx(input_text.text)
     return {
         "text": input_text.text,
         "sentiment": sentiment,
         }
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8080, log_level="info")
